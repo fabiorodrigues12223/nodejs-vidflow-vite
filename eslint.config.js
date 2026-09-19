c0c477c5679e86ed768549379e5074ce59152ddf
Fábio Rodrigues <fabio1203.fabio@gmail.com>
2026-09-19 17:48:25 -0300

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+// eslint.config.js
+module.exports = [
+  {
+    files: ["**/*.js"],
+
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+
+    rules: {
+      // suas regras vão aqui, pode adicionar conforme precisar
+    },
+  },
+];
